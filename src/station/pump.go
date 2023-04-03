5ae57a445166d29a49e22b2b97fa89904087e8f0
Nikita Tashchilin <nikita.tashilin@gmail.com>
2023-04-03 13:52:59 +0200

    fix: edit pump nipples logic

--- src/station/pump.go
+++ src/station/pump.go
@@ -4,48 +4,36 @@ import (
 	"fmt"
 	"gas-station-simulator/src/car"
 	"gas-station-simulator/src/fuel"
+	"sync"
 	"time"
 )
 
 type Pump struct {
 	Type    fuel.Fuel
 	Queue   chan *car.Car
-	Nipples []chan *car.Car
+	Nipples int
 }
 
-func (p Pump) refuel(ch chan *car.Car, c *car.Car, checkouts chan *car.Car) {
+func (p Pump) refuel(c *car.Car, checkouts chan *car.Car) {
 	fmt.Printf("⛽️  %v started refueling at %v, wait for %v\n", c, time.Now().Format("15:04:05.000"), c.RefuelDuration)
 	c.Refuel()
 	c.RefueledAt = time.Now()
 	fmt.Printf("🤑  %v successfully refueled, moved to checkout..\n", c)
-	checkouts <- <-ch
+	checkouts <- c
 }
 
 func (p Pump) Run(checkouts chan *car.Car) {
-	for c := range p.Queue {
-		isWaiting := true
-		for isWaiting {
-			for _, ch := range p.Nipples {
-				select {
-				case ch <- c:
-					isWaiting = false
-					go p.refuel(ch, c, checkouts)
-				default:
-				}
-				if !isWaiting {
-					break
-				}
+	wg := &sync.WaitGroup{}
+	wg.Add(p.Nipples)
+	for i := 0; i < p.Nipples; i++ {
+		go func() {
+			defer wg.Done()
+			for c := range p.Queue {
+				p.refuel(c, checkouts)
 			}
-		}
+		}()
 	}
-}
-
-func genNipples(count int) []chan *car.Car {
-	nipples := make([]chan *car.Car, count)
-	for i := range nipples {
-		nipples[i] = make(chan *car.Car, 1)
-	}
-	return nipples
+	wg.Wait()
 }
 
 func GeneratePumps() map[fuel.Fuel]Pump {
@@ -53,22 +41,22 @@ func GeneratePumps() map[fuel.Fuel]Pump {
 	pumps[fuel.Gas] = Pump{
 		Type:    fuel.Gas,
 		Queue:   make(chan *car.Car, 3),
-		Nipples: genNipples(4),
+		Nipples: 4,
 	}
 	pumps[fuel.Diesel] = Pump{
 		Type:    fuel.Diesel,
 		Queue:   make(chan *car.Car, 3),
-		Nipples: genNipples(4),
+		Nipples: 4,
 	}
 	pumps[fuel.LPG] = Pump{
 		Type:    fuel.LPG,
 		Queue:   make(chan *car.Car, 2),
-		Nipples: genNipples(1),
+		Nipples: 4,
 	}
 	pumps[fuel.Electric] = Pump{
 		Type:    fuel.LPG,
 		Queue:   make(chan *car.Car, 2),
-		Nipples: genNipples(8),
+		Nipples: 4,
 	}
 	return pumps
 }
